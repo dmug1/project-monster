d0a05bb1c71aaa85b0694c4320978bfd6bba978b
Diego <diegomugnae@gmail.com>
2020-03-23 21:09:56 +1000

    Create heal method

--- app.js
+++ app.js
@@ -13,24 +13,35 @@ new Vue ({
     },
     methods:{
         startGame() {
-            this.running = true
-            this.playerLife = 100
+            this.running = true;
+            this.playerLife = 100;
             this.monsterLife = 100;
         },
         attack(special){
             console.log('attack')
-            this.hurt('playerLife',5,10,false)            
-            this.hurt('monsterLife',7,5,special)
+            /*this.hurt('playerLife',7,12,false)       */
+            setTimeout(() => this.hurt('playerLife',7,12,false), 500)
+            this.hurt('monsterLife',5,10,special)
+            /*setTimeout(() => this.hurt('monsterLife',7,5,special), 500);     */
+            /*this.hurt('monsterLife',5,10,special)*/
         },
         hurt(toBeAttacked,min,max,special){
             const plus = special ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[toBeAttacked] = Math.max(this[toBeAttacked] - hurt, 0) //make sure minimun value is 0
         },
+        heal(min, max){
+           const heal = this.getRandom(min, max);
+           setTimeout(() => this.playerLife = Math.min(this.playerLife + heal, 100),500);
+        },
+        healAndHurt(){
+            this.heal(10, 15);            
+            this.hurt('playerLife',7,12,false)
+        },
         getRandom(min, max){
             const value = Math.random() * (max-min) + min
             return Math.round(value)
-        }
+        },
 
     },
     watch:{
