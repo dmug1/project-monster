335d877d2f1037fa98afacd9b8c70711551e799b
Diego <diegomugnae@gmail.com>
2020-03-23 19:35:05 +1000

    do Result panel

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 new Vue ({
     el: '#app',
     data: {
-        playerLife: 100,
+        playerLife: 0,
         monsterLife: 100,
 
     },
     computed:{
-
+        hasResults(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods:{
 
