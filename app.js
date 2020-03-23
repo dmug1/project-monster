e2f2d2cf8a420f81838f5dfd92dc681a934c555d
Diego <diegomugnae@gmail.com>
2020-03-23 21:27:29 +1000

    Registering battle log

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue ({
         playerLife: 100,
         monsterLife: 100,
         running: false,
+        logs: [],
 
     },
     computed:{
@@ -16,19 +17,23 @@ new Vue ({
             this.running = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+            this.log = [];
         },
         attack(special){
-            console.log('attack')
             /*this.hurt('playerLife',7,12,false)       */
-            setTimeout(() => this.hurt('playerLife',7,12,false), 500)
-            this.hurt('monsterLife',5,10,special)
+            this.hurt('monsterLife',5,10,special,'Player', 'Monster', 'playerCls')
+            if(this.monsterLife > 0){
+                setTimeout(() => this.hurt('playerLife',7,12,false,'Monster','Player','monsterCls'), 500)
+            }
             /*setTimeout(() => this.hurt('monsterLife',7,5,special), 500);     */
             /*this.hurt('monsterLife',5,10,special)*/
         },
-        hurt(toBeAttacked,min,max,special){
+
+        hurt(toBeAttacked,min,max,special,source,target, cls){
             const plus = special ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[toBeAttacked] = Math.max(this[toBeAttacked] - hurt, 0) //make sure minimun value is 0
+            this.logRegister(`${target} has been hitted by ${source} and lost ${hurt}HP. `,cls)
         },
         heal(min, max){
            const heal = this.getRandom(min, max);
@@ -42,6 +47,9 @@ new Vue ({
             const value = Math.random() * (max-min) + min
             return Math.round(value)
         },
+        logRegister(text, cls){
+            this.logs.unshift( {text, cls } )
+        }
 
     },
     watch:{
