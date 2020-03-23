9bcef6ea6dd431b8f3903470e9e4354add5a7123
Diego <diegomugnae@gmail.com>
2020-03-23 20:53:09 +1000

    Create methods for attack/special
    Methods Hurt, attack and GetRandom

--- app.js
+++ app.js
@@ -16,10 +16,26 @@ new Vue ({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100;
+        },
+        attack(special){
+            console.log('attack')
+            this.hurt('playerLife',5,10,false)            
+            this.hurt('monsterLife',7,5,special)
+        },
+        hurt(toBeAttacked,min,max,special){
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[toBeAttacked] = Math.max(this[toBeAttacked] - hurt, 0) //make sure minimun value is 0
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max-min) + min
+            return Math.round(value)
         }
 
     },
     watch:{
-
+        hasResults(value) {
+            if(value) this.running = false
+        }
     }
 })
